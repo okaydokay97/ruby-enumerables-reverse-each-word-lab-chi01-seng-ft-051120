66903bd41c76121c33384916ce4df94c00ce1549
Rubens Fernandes <rubens.fernandes97@gmail.com>
2020-04-27 04:44:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,9 +6,9 @@
 #   new_array.join(" ")
 # end
 def reverse_each_word(string)
-  string = string.split(/ /)
+  string =string.split(/ /)
   reverse = string.collect do |words|
     words.reverse 
   end
-  new_array.join(" ")
+  reverse.join(" ")
 end
